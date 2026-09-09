0e06a8e7d16d9c0ea5f80fb7f1d607342292fd95
juniorcalves87 <90777024+juniorcalves87@users.noreply.github.com>
2026-09-09 13:25:50 -0300

    feat(mfv): add annual map activity categories

---
+++ mfv-java/src/main/java/br/com/mfv/pcm/ActivityCategory.java
@@ -0,0 +1,15 @@
+package br.com.mfv.pcm;
+
+public enum ActivityCategory {
+  PM,
+  PDM,
+  INSPECAO,
+  CORRETIVA_PLANEJADA,
+  SHUTDOWN,
+  PROJETO_CAPEX,
+  TERCEIROS,
+  MATERIAIS,
+  MAO_DE_OBRA,
+  TREINAMENTO,
+  COMPLIANCE
+}
